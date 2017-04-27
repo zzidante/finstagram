22e0a47de3147b4ecc6a236f31d19344f8c04cb6
ZziDante <zzidante@gmail.com>
2017-04-27 03:19:43 +0000

    Added Signup Form page, connected to database

---
+++ app/models/post.rb
@@ -0,0 +1,27 @@
+class Post < ActiveRecord::Base
+    
+    belongs_to :user
+    has_many :comments
+    has_many :likes
+    
+    def humanized_time_ago
+        time_ago_in_seconds = Time.now - self.created_at
+        time_ago_in_minutes = time_ago_in_seconds / 60
+        
+        if time_ago_in_minutes >= 60
+            "#{(time_ago_in_minutes / 60).to_i} hours ago"
+        else
+            "#{time_ago_in_minutes.to_i} minutes ago."
+        end
+    end
+        
+        
+    def like_counts
+        self.likes.size
+    end
+    
+    def comment_count
+        self.comments.size
+    end
+    
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+    
+    has_many :posts
+    has_many :comments
+    has_many :likes
+    
+    validates_presence_of :email, :avatar_url, :username, :password
+    validates_uniqueness_of :email, :username 
+    
+    
+end
+
